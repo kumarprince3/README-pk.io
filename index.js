dcf8ddfe871bd5c254fdf84a87f5d438c9b33cfd
Prince Kumar <kumarprincebiz@gmail.com>
2020-06-06 11:09:46 -0500

    added content to index

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-index.js
+var inquirer = require('inquirer');
+var fs = require('fs');
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'first',
+        message: 'Hello! Type your first name please.'
+    },
+    {
+        type: 'input',
+        name: 'second',
+        message: 'Hello! Type your first name please.'
+    },
+    {
+        type: 'input',
+        name: 'third',
+        message: 'Hello! Type your first name please.'
+    },
+    {
+        type: 'input',
+        name: 'fourth',
+        message: 'Hello! Type your first name please.'
+    }
+])
+.then(function(answers) {
+    console.log('What is this?', answers);
+    var readMeString = `
+    Title ${answers.first}
+    Description ${answer.second}
+    `
+    fs.writeFile('README.md', readMeString, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+})
